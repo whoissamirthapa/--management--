cd4516a9fd1d214e6d4854545fae571c02579f78
Samir Thapa <59677197+thapasamirm577@users.noreply.github.com>
2021-07-17 20:37:34 +0545

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -3,7 +3,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$database = "phpTraining";
+$database = "management";
 
 $conn = mysqli_connect($servername, $username, $password, $database);
 
